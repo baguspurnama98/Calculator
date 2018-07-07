b7f8277f94c794de943544105bdbbcae47106c2e
Cornelius Bagus Purnama Putra <40450872+baguspurnama98@users.noreply.github.com>
2018-07-07 21:15:27 +0700

    Create bg1.java
    the simple calculator

---
+++ bg1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class bg1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        char c = sc.next().charAt(0);
+        if(c == '+'){
+            System.out.println(a+b);
+        }
+        else if(c == '-'){
+            System.out.println(a-b);
+        }
+        else if(c == '*'){
+            System.out.println(a*b);
+        }
+        else if(c == '/'){
+            System.out.println(a/b);
+        }
+    }
+}
+
+
